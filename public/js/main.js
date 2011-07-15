d4b6b31eea77cd39d387f515ced18f73b508854f
Sho Hashimoto <hashimoto@shokai.org>
2011-07-15 23:57:49 +0900

    escape html

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
+String.prototype.htmlEscape = function(){
+    var span = document.createElement('span');
+    var txt =  document.createTextNode('');
+    span.appendChild(txt);
+    txt.data = this;
+    return span.innerHTML;
+};
+
 $(function(){
     $('input#start_btn').click(omikuji_start);
 });
@@ -5,7 +13,7 @@ $(function(){
 var omikuji_start = function(){
     $.getJSON(omikuji_api, function(res){
         console.log(res);
-        var li = $('<li>').append(res.result + ' - ' + res.time);
+        var li = $('<li>').append(res.result.htmlEscape() + ' - ' + res.time.htmlEscape());
         $('ul#results').prepend(li);
     });
 };
