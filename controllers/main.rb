aaf44c29f9571fce68740b62641dff8f54c14c10
Sho Hashimoto <hashimoto@shokai.org>
2011-12-22 21:25:12 +0900

    fix json content_type

--- controllers/main.rb
+++ controllers/main.rb
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+before '/*.json' do
+  content_type 'application/json'
+end
+
 get '/' do
   @title = @@conf['title']
   haml :index
 end
 
 get '/omikuji.json' do
-  content_type = 'application/json'
   arr = ['大吉', '中吉', '小吉', 'マジキチ', '凶']
   @mes = {
     :result => arr.choice,
