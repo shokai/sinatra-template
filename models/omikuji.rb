2880d77577378b9266ada6531a4148b00ab0e67e
Sho Hashimoto <hashimoto@shokai.org>
2012-04-30 22:41:34 +0900

    move db-query into model

--- models/omikuji.rb
+++ models/omikuji.rb
@@ -3,4 +3,16 @@ class Omikuji
   include Mongoid::Document
   field :time, :type => Time , :default => lambda{Time.now}
   field :result, :type => String, :default => ''
+
+  def self.last
+    self.all.desc(:time).limit(1).first
+  end
+
+  def self.find_by_result(pattern)
+    self.where(:result => pattern)
+  end
+
+  def self.latests(num=10)
+    self.all.desc(:time).limit(num)
+  end
 end
