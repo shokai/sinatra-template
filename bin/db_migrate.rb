76cb4405651ef3d4c3f286f91216ba3c3f8daa4a
Sho Hashimoto <hashimoto@shokai.org>
2012-05-14 01:31:25 +0900

    add bin/db_upgrade

---
+++ bin/db_migrate.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require 'yaml'
+require 'dm-migrations'
+
+begin
+  @@conf = YAML::load open(File.dirname(__FILE__)+'/../config.yaml').read
+  p @@conf
+rescue => e
+  STDERR.puts 'config.yaml load error!'
+  STDERR.puts e
+  exit 1
+end
+
+[:inits, :models].each do |dir|
+  Dir.glob(File.dirname(__FILE__)+"/../#{dir}/*.rb").each do |rb|
+    puts "loading #{rb}"
+    require rb
+  end
+end
+
+DataMapper.auto_migrate!
--- bin/db_upgrate.rb
+++ bin/db_upgrate.rb
@@ -20,5 +20,4 @@ end
   end
 end
 
-DataMapper.auto_migrate!
 DataMapper.auto_upgrade!
