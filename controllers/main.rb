29e811bbdfe8eaa6907d43f33eb08e2575b2de31
Sho Hashimoto <hashimoto@shokai.org>
2012-05-15 03:07:27 +0900

    move omikuji data into config.yml

--- controllers/main.rb
+++ controllers/main.rb
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 before '/*.json' do
   content_type 'application/json'
 end
@@ -9,9 +8,8 @@ get '/' do
 end
 
 get '/omikuji.json' do
-  arr = ['大吉', '中吉', '小吉', 'マジキチ', '凶']
   @mes = {
-    :result => arr.choice,
+    :result => Conf['omikuji'].choice,
     :time => Time.now.to_s
   }.to_json
 end
