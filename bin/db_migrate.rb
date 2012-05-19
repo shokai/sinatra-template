f55fa202547d0e4b51567f36b46fce68f904dd9d
Sho Hashimoto <hashimoto@shokai.org>
2012-05-19 16:10:48 +0900

    add caution message in db_migrate.rb

--- bin/db_migrate.rb
+++ bin/db_migrate.rb
@@ -5,6 +5,7 @@ require 'dm-migrations'
 require File.dirname(__FILE__)+'/../bootstrap'
 Bootstrap.init :inits, :models
 
+puts 'migration will DELETE and RESET your DB.'
 puts 'initialize DB?'
 puts '[y/N]'
 exit unless STDIN.gets =~ /y/i
