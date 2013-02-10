f28c0c46125f022a3fdabc3b075fc4de02868816
Sho Hashimoto <hashimoto@shokai.org>
2013-02-11 00:03:40 +0900

    fix helper

--- helpers/helper.rb
+++ helpers/helper.rb
@@ -1,4 +1,5 @@
-
-def app_root
-  "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
+helpers do
+  def app_root
+    "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
+  end
 end
